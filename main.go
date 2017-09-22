ed98cc09d1c2ee4d9e29cc2e0389761de47b18d0
Jerzy Biernacki <jerzy.biernacki@gmail.com>
2017-09-22 22:28:17 +0200

    Basic server implementation - serves frontend only for now

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func validateFrontend() {
+	if _, err := os.Stat("frontend/build/index.html"); os.IsNotExist(err) {
+		log.Fatal("ERROR: Frontend root file does not exist. Make sure to run 'yarn build' first. ", err)
+	}
+}
+
+func main() {
+	validateFrontend()
+	log.Print("Starting server...")
+
+	http.Handle("/", http.FileServer(http.Dir("frontend/build/"))) //TODO: Add option to not to serve Frontend by Go
+
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	http.ServeFile(w, r, "frontend/build/index.html")
+	// })
+	// http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+	// 	serveWs(hub, w, r)
+	// })
+	err := http.ListenAndServe("localhost:8080", nil) //TODO: Parameterize host and port
+	if err != nil {
+		log.Fatal("Could not start server: ", err)
+	}
+}
